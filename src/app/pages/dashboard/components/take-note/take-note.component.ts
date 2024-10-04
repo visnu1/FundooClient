759fae918965350255fcefc818ea813584fdb1a1
Vishnu Ranjan <visnu@mac.mynetworksettings.com>
2024-10-03 20:53:20 -0400

    added take notes list feature

--- src/app/pages/dashboard/components/take-note/take-note.component.ts
+++ src/app/pages/dashboard/components/take-note/take-note.component.ts
@@ -1,9 +1,20 @@
-import { Component, OnInit, Output, EventEmitter, DoCheck } from '@angular/core';
-import { Router } from '@angular/router';
+import { Component, OnInit, Output, EventEmitter, Renderer2, ElementRef, ViewChild } from '@angular/core';
 import { NoteService } from '../../../../core/services/note/note.service';
 import { DataService } from '../../../../core/services/data-service/data.service';
 
 
+interface Note {
+  userId: string,
+  title: string,
+  description: string,
+  reminder: string,
+  color: string,
+  archive: boolean,
+  trash: boolean,
+  pinned: boolean,
+  labels: string[],
+  noteType: 'plain' | 'list'
+}
 @Component({
   selector: 'app-take-note',
   templateUrl: './take-note.component.html',
@@ -13,77 +24,156 @@ import { DataService } from '../../../../core/services/data-service/data.service
 
 export class TakeNoteComponent implements OnInit {
 
-
   color: string = "#fff";
   archive: boolean = false;
   trash: boolean = false;
   pinned: boolean = false;
   rem: any = "";
   userNoteMsg = true;
-  hide: boolean = false;
+  hidden = true;
   labels: string[] = [];
-  noteType = 'plain';
 
-  constructor(
-    private service: NoteService,
-    private router: Router,
-    private data: DataService) { }
-
-  ngOnInit() {
-  }
+  @ViewChild('noteTitle') noteTitle!: ElementRef;
+  @ViewChild('listNote') listNote!: ElementRef;
+  @ViewChild('plainNote') userNotes!: ElementRef;
 
   @Output() addingNote = new EventEmitter();
 
-  send() {
-    this.toggleNotes();
-    if (document.getElementById("asas").innerText != "" || document.getElementById("title").innerText != "") {
-      // console.log(this.data.userid);
-      let body = {
-        userId: this.data.userId,
-        title: document.getElementById("title").innerText,
-        description: document.getElementById("asas").innerText,
-        reminder: this.rem,
-        color: this.color,
-        archive: this.archive,
-        trash: this.trash,
-        pinned: this.pinned,
-        labels: this.labels,
-        noteType: this.noteType
-      }
-      this.service.createNote(body).subscribe(data => {
-        console.log(data);
-        this.addingNote.emit();
-      }, errors => {
-        if (errors.status == 500) {
-          console.error(errors.error.message + '\n\t' + errors.error.result);
+  noteType: 'plain' | 'list' = 'plain';
+  prevCheckListIpList;
 
-        } else if (errors.status == 422) {
-          console.warn("Invalid inputs ");
-        }
-        else {
-          console.error(errors);
-        }
-      })
-    } else {
-      this.color = "#fff";
-      return;
-    }
-    this.color = "#fff";;
-  }
+  constructor(
+    private service: NoteService,
+    private data: DataService,
+    private renderer: Renderer2
+  ) { }
 
-  onNewNoteList() {
-    this.noteType = 'list';
-    this.toggleNotes();
-    
-  }
+  ngOnInit() { }
+
 
   toggleNotes() {
-    this.hide = !this.hide;
+    this.hidden = !this.hidden;
+  }
+
+  newListNote() {
+    this.noteType = 'list';
+    this.toggleNotes();
+    if (!this.hidden)
+      setTimeout(() => this.createTodoFormFld(), 50);
+  }
+
+
+  onSend() {
+
+    if (this.noteType == 'list') this.extractTodoItems();
+
+    const noteTitle = this.noteTitle.nativeElement.innerText || "";
+    const userNotes = this.userNotes.nativeElement.innerText || "";
+
+    if (!(noteTitle != "" || userNotes != "")) return;
+
+    const body: Note = {
+      userId: this.data.userId,
+      title: noteTitle,
+      description: userNotes,
+      reminder: this.rem,
+      color: this.color,
+      archive: this.archive,
+      trash: this.trash,
+      pinned: this.pinned,
+      labels: this.labels,
+      noteType: this.noteType
+    }
+
+    this.service.createNote(body).subscribe({
+      next: (data) => this.addingNote.emit(),
+      error: (e) => {
+        if (e.status == 500)
+          console.error(e.error.message + '\n\t' + e.error.result);
+        else if (e.status == 422)
+          console.warn("Invalid inputs ");
+        else
+          console.error(e);
+      },
+      complete: () => { this.color = "#fff"; this.noteType = 'plain'; this.toggleNotes(); }
+    });
+  }
+
+  extractTodoItems() {
+    // needs implementation
+  }
+
+  createTodoFormFld() {
+    // Cleanup previous listeners
+    if (this.prevCheckListIpList)
+      this.prevCheckListIpList();
+
+    const frmCtn = this.renderer.createElement('div');
+    const frm = this.renderer.createElement('div');
+
+    this.renderer.addClass(frmCtn, 'frm-ctn');
+    this.renderer.addClass(frm, 'frm');
+    this.renderer.setAttribute(frm, 'contenteditable', 'true');
+    this.renderer.setAttribute(frm, 'aria-multiline', 'true');
+    this.renderer.setAttribute(frm, 'tabindex', '0');
+
+    this.renderer.setAttribute(frmCtn, 'data-complete', 'false');
+
+    // Bind the input event and store the cleanup function
+    this.prevCheckListIpList = this.renderer.listen(frm, 'input', (event) => this.onAddTodoItem(event));
+
+    // Append elements
+    this.renderer.appendChild(frmCtn, frm);
+    this.renderer.appendChild(this.listNote.nativeElement, frmCtn);
+
+    //focus
+    // this.renderer.selectRootElement(frm).focus();
+  }
+
+  createTodoCtrls(parentEle: HTMLElement) {
+    const checkBox = this.renderer.createElement('span');
+    this.renderer.setAttribute(checkBox, 'class', 'material-symbols-outlined small-icon prefix-ico');
+    this.renderer.setAttribute(checkBox, 'aria-checked', 'false');
+    this.renderer.setAttribute(checkBox, 'role', 'checkbox');
+    // this.renderer.setAttribute(checkBox, 'tabindex', '0');
+    this.renderer.appendChild(checkBox, this.renderer.createText('check_box_outline_blank'));
+
+    const closeIcon = this.renderer.createElement('span');
+    this.renderer.setAttribute(closeIcon, 'class', 'material-symbols-outlined small-icon suffix-ico');
+    // this.renderer.setAttribute(closeIcon, 'tabindex', '0');
+    this.renderer.appendChild(closeIcon, this.renderer.createText('close'));
+
+    this.renderer.appendChild(parentEle, checkBox);
+    this.renderer.appendChild(parentEle, closeIcon);
+
+    //add event listeners
+    this.renderer.listen(closeIcon, 'click', ($event) => this.onRemoveTodoFld($event.target.parentElement));
+    this.renderer.listen(checkBox, 'click', ($event) => this.onTodoCheckbox($event.target))
+  }
+
+  onAddTodoItem(event: Event): void {
+    const target = (event.target as HTMLElement);
+    this.createTodoCtrls(target.parentElement)
+    this.createTodoFormFld();
+  }
+
+  onTodoCheckbox(checkbox: HTMLElement) {
+    const parentEle = checkbox.parentElement;
+    // Get the current 'data-complete' status as a boolean
+    const isChecked = parentEle.getAttribute('data-complete') === 'true';
+
+    this.renderer.setProperty(checkbox, 'innerText', isChecked ? 'check_box_outline_blank' : 'check_box');
+    this.renderer.setAttribute(checkbox, 'aria-checked', isChecked ? 'false' : 'true');
+    this.renderer.setAttribute(parentEle, 'data-complete', isChecked ? 'false' : 'true');
+  }
+
+  onRemoveTodoFld(parentEle: HTMLElement) {
+    this.renderer.removeChild(this.listNote.nativeElement, parentEle);
   }
 
   userArchive(event) {
     this.archive = event;
-    this.send();
+    this.onSend();
   }
 
   noteColor(color) {
@@ -93,4 +183,5 @@ export class TakeNoteComponent implements OnInit {
   reminder(date) {
     this.rem = date;
   }
+
 }
